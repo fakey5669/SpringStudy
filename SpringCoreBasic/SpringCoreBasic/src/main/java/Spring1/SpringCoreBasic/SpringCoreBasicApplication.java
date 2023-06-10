bf479017f15750062e99e2a2336ee72eb5589fc2
SeoWonKyu <fakey5669@gmail.com>
2023-06-11 01:16:03 +0900

    스프링 핵심 원리-기본편 프로젝트 생성 스프링부트로 생성 spring-2.7.1, java-17

---
+++ SpringCoreBasic/SpringCoreBasic/src/main/java/Spring1/SpringCoreBasic/SpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package Spring1.SpringCoreBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreBasicApplication.class, args);
+	}
+
+}
---
+++ SpringCoreBasic/SpringCoreBasic/src/test/java/Spring1/SpringCoreBasic/SpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring1.SpringCoreBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
