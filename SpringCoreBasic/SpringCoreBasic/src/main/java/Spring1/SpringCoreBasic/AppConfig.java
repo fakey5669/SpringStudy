6bf6703f78a1bd9f19f5c78eb50ad441dd75475d
SeoWonKyu <fakey5669@gmail.com>
2023-06-26 17:16:29 +0900

    스프링으로 전환 AnnotationConfigApplicationContext과 @Configuration @Bean으로 스프링으로 전환 모든 빈 조회 만듦.

--- SpringCoreBasic/SpringCoreBasic/src/main/java/Spring1/SpringCoreBasic/AppConfig.java
+++ SpringCoreBasic/SpringCoreBasic/src/main/java/Spring1/SpringCoreBasic/AppConfig.java
@@ -1,19 +1,36 @@
 package Spring1.SpringCoreBasic;
 
-import Spring1.SpringCoreBasic.discount.FixDiscountPolicy;
+import Spring1.SpringCoreBasic.discount.DiscountPolicy;
+import Spring1.SpringCoreBasic.discount.RateDiscountPolicy;
+import Spring1.SpringCoreBasic.member.MemberRepository;
 import Spring1.SpringCoreBasic.member.MemberService;
 import Spring1.SpringCoreBasic.member.MemberServiceImpl;
 import Spring1.SpringCoreBasic.member.MemoryMemberRepository;
 import Spring1.SpringCoreBasic.order.OrderService;
 import Spring1.SpringCoreBasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+  @Bean
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
   }
 
+  @Bean
+  public MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+
+  @Bean
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  @Bean
+  public DiscountPolicy discountPolicy() {
+    return new RateDiscountPolicy();
   }
 }
--- SpringCoreBasic/SpringCoreBasic/src/main/java/Spring1/SpringCoreBasic/MemberApp.java
+++ SpringCoreBasic/SpringCoreBasic/src/main/java/Spring1/SpringCoreBasic/MemberApp.java
@@ -3,14 +3,18 @@ package Spring1.SpringCoreBasic;
 import Spring1.SpringCoreBasic.member.Grade;
 import Spring1.SpringCoreBasic.member.Member;
 import Spring1.SpringCoreBasic.member.MemberService;
-import Spring1.SpringCoreBasic.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
   public static void main(String[] args) {
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
-    OrderService orderService = appConfig.orderService();
+//    AppConfig appConfig = new AppConfig();
+//    MemberService memberService = appConfig.memberService();
+
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
     Member member = new Member(1L, "memberA", Grade.VIP);
     memberService.join(member);
     Member findMember = memberService.findMember(1L);
