6d08448cbe2daa0c88b82821f1e103e942184286
fakey5669 <fakey5669@gmail.com>
2023-08-14 22:08:27 +0900

    08-14 롬복과 최신 트렌드

---
+++ SpringCoreBasic/SpringCoreBasic/src/main/java/Spring1/SpringCoreBasic/HelloLombok.java
@@ -0,0 +1,16 @@
+package Spring1.SpringCoreBasic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("fsadk");
+    }
+}
